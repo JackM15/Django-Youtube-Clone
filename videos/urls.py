a3bdedb675097d8f10428c366097b5316a4ff239
Jack M <jackm550@googlemail.com>
2022-05-18 11:54:09 +0100

    video player now working with video.js and a page created for each video to display

--- videos/urls.py
+++ videos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateVideoView
+from .views import CreateVideoView, DetailVideo
 
 urlpatterns = [
     path("create/", CreateVideoView.as_view(), name="video-create"),
+    path("<int:pk>/", DetailVideo.as_view(), name="video-detail"),
 ] 
--- videos/views.py
+++ videos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 
 from .models import Video
 
@@ -12,5 +13,10 @@ class CreateVideoView(CreateView):
     fields = ['title', 'description', 'video_file', 'thumbnail']
     template_name = 'videos/create_video.html'
 
-    # def get_success_url(self):
-    #     return reverse("video-detail", kwargs={"pk" : self.object.pk})
+    def get_success_url(self):
+        return reverse("video-detail", kwargs={"pk" : self.object.pk})
+
+
+class DetailVideo(DetailView):
+    model = Video
+    template_name = 'videos/detail_video.html'
