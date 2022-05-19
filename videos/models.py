c7dd7111b9f8b01587df4692710309b6d990af76
Jack M <jackm550@googlemail.com>
2022-05-19 09:22:38 +0100

    Associated the user with the video model and prevented users from editing/deleting videos that arent their own.

--- videos/models.py
+++ videos/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import FileExtensionValidator
+from django.contrib.auth.models import User
 
 class Video(models.Model):
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
     video_file = models.FileField(upload_to="uploads/video_files", validators=[FileExtensionValidator(allowed_extensions=['mp4'])])
     thumbnail = models.FileField(upload_to="uploads/thumbnails", validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
     date_posted = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return self.title
--- videos/views.py
+++ videos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 
@@ -14,6 +14,11 @@ class CreateVideoView(LoginRequiredMixin, CreateView):
     fields = ['title', 'description', 'video_file', 'thumbnail']
     template_name = 'videos/create_video.html'
 
+    def form_valid(self, form):
+        # Assign the current user to the new video
+        form.instance.uploader = self.request.user
+        return super().form_valid(form)
+
     def get_success_url(self):
         return reverse("video-detail", kwargs={"pk" : self.object.pk})
 
@@ -23,18 +28,26 @@ class DetailVideo(DetailView):
     template_name = 'videos/detail_video.html'
 
 
-class UpdateVideo(LoginRequiredMixin, UpdateView):
+class UpdateVideo(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Video
     fields = ['title', 'description']
     template_name = 'videos/create_video.html'
 
     def get_success_url(self):
         return reverse("video-detail", kwargs={"pk" : self.object.pk})
+    
+    def test_func(self):
+        video = self.get_object()
+        return self.request.user == video.uploader
 
 
-class DeleteVideo(LoginRequiredMixin, DeleteView):
+class DeleteVideo(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Video
     template_name = 'videos/delete_video.html'
 
     def get_success_url(self):
         return reverse("index")
+
+    def test_func(self):
+        video = self.get_object()
+        return self.request.user == video.uploader
